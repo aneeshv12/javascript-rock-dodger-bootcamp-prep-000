5ac77151024debdfd46e17db113bf5d5e333fd8a
Aneesh Venuturumilli <veeaneesh@gmail.com>
2019-01-07 21:50:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -169,7 +169,7 @@ function moveDodgerRight() {
    */
    var leftNumbers = dodger.style.left.replace('px', '');
    var left = parseInt(leftNumbers, 10);
-   if (left > 0) {
+   if (left < 360) {
     dodger.style.left = `${left + 4}px`
   }
 }
