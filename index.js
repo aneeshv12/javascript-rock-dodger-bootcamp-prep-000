da0cb63b6c6bcce9f701e7a8befed7bc298161b2
Aneesh Venuturumilli <veeaneesh@gmail.com>
2019-01-07 21:46:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -155,6 +155,7 @@ function moveDodgerLeft() {
    * This function should move DODGER to the left
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
+   var leftNumbers = dodger.style.left.replace('px', '')
 }
 
 function moveDodgerRight() {
