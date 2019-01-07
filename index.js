117abf8355e49864829fc3ced51ae68fac9fe077
Aneesh Venuturumilli <veeaneesh@gmail.com>
2019-01-07 21:48:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -136,15 +136,17 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
-   if (e.which === LEFT_ARROW){
-       e.preventDefault()
-       e.stopPropagation()
-       moveDodgerLeft()
-     }else if (e.which === RIGHT_ARROW){
-       e.preventDefault()
-       e.stopPropagation()
-       moveDodgerRight()
-     }
+   if(e.which() === LEFT_ARROW){
+     e.preventDefault();
+     e.stopPropagation();
+     moveDodgerLeft();
+   }else if(e.which() === RIGHT_ARROW){
+     e.preventDefault();
+     e.stopPropagation();
+     moveDodgerRight();
+   }else{
+     return;
+   }
 }
 
 function moveDodgerLeft() {
